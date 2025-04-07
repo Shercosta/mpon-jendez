6cca5027361af24c0d02a79d7e42b4cf664761fd
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2025-04-07 19:47:24 +0700

    feat: add 'bgColor' property to items for improved visual distinction

--- js/items.js
+++ js/items.js
@@ -11,6 +11,7 @@ export const items = [
     isCommingSoon: false,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    bgColor: "#f8d7da",
   },
   {
     code: "jsr",
@@ -29,6 +30,7 @@ export const items = [
     isCommingSoon: false,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    bgColor: "#f8d7da",
   },
   {
     code: "rtg",
@@ -42,5 +44,6 @@ export const items = [
     isCommingSoon: true,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    bgColor: "#f8d7da",
   },
 ];
